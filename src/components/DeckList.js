dd1049345fd52265a555da01e9f37de9feba204a
Alfio Parisi <alfioparisi.93@gmail.com>
2017-12-10 17:38:25 +0000

    Fix DeckList

--- src/components/DeckList.js
+++ src/components/DeckList.js
@@ -3,40 +3,26 @@ import { View, FlatList, TouchableOpacity, StyleSheet, Dimensions } from 'react-
 import MainHeader from './MainHeader';
 import DeckListItem from './DeckListItem';
 import { Feather } from '@expo/vector-icons';
-
-import store from '../store';
+import { connect } from 'react-redux';
 
 class DeckList extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      decks: []
-    }
-  }
-
-  componentDidMount() {
-    // Get the initial decks (eventually from AsyncStorage)
-    const decks = store.getState().decks;
-    // Format the data because FlatList wants an array of objects with a 'key' property.
-    const listData = Object.keys(decks).map(item => ({key: item}));
-    return this.setState({
-      decks: listData
-    })
   }
 
   render() {
-    const { decks } = this.state;
-    const { navigation } = this.props;
+    const { decks, navigation } = this.props;
+    const decksList = Object.keys(decks).map(item => ({key: item}));
     return (
       <View style={styles.container}>
         <MainHeader />
         <FlatList
           contentContainerStyle={styles.deckList}
-          data={decks}
+          data={decksList}
           renderItem={({ item }) => (
             <DeckListItem
               deck={item.key}
-              cardsNumber={store.getState().decks[item.key].questions.length}
+              cardsNumber={decks[item.key].questions.length}
               navigation={navigation}
             />
           )}
@@ -73,4 +59,8 @@ const styles = StyleSheet.create({
   }
 });
 
-export default DeckList;
+const mapStateToProps = state => ({
+  decks: state.decks
+});
+
+export default connect(mapStateToProps)(DeckList);
