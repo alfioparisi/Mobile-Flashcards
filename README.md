76bba941cc499b8765c9edca3aa1c3e48c2d9b8e
Alfio Parisi <alfioparisi.93@gmail.com>
2017-12-20 09:56:44 +0000

    Update ReadMe.

--- README.md
+++ README.md
@@ -8,5 +8,14 @@ Its purpose is to help in the study by playing quizzes. Users can create their o
 
 ## Run the app
 
-To run the app you'll need [Expo](https://docs.expo.io/versions/latest/index.html) on your mobile phone or emulator.
-Once you run `npm start` and the app is running open Expo and scan the QR code in the terminal. The app will finally open.
+To run the app first you need to install dependencies. Go to your terminal and `cd`
+into the app folder. Then run `npm install`. Once that's done `npm start` will make
+the app work.
+
+You'll also need [Expo](https://docs.expo.io/versions/latest/index.html) on your mobile phone or emulator.
+Once you run `npm start` and the app is running open Expo and scan the QR code in the terminal.
+The app will finally open.
+
+### Developing and testing
+
+The app was developed and tested on Huawei P8 Lite.
--- src/components/DeckList.js
+++ src/components/DeckList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, FlatList, TouchableOpacity, StyleSheet, AsyncStorage } from 'react-native';
 import DeckListItem from './DeckListItem';
 import { Ionicons } from '@expo/vector-icons';
@@ -8,39 +8,32 @@ import { connect } from 'react-redux';
   Render the list of all available decks and the button to create a new deck.
   @param {object} : decks
 */
-class DeckList extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { decks, navigation } = this.props;
-    const decksList = Object.keys(decks).map(item => ({key: item}));
-    return (
-      <View style={styles.container}>
-        {decks && (
-          <FlatList
-            contentContainerStyle={styles.deckList}
-            data={decksList}
-            renderItem={({ item }) => (
-              <DeckListItem
-                deck={item.key}
-                cardsNumber={decks[item.key].questions.length}
-                navigation={navigation}
-              />
-            )}
-          />
-        )}
-        <TouchableOpacity
-          style={styles.addBtn}
-          onPress={() => navigation.navigate('CreateDeck')}
-        >
-          <Ionicons name='md-add-circle' size={55} color='#f83926' />
-        </TouchableOpacity>
-      </View>
-    );
-  }
-}
+const DeckList = ({ decks, navigation }) => {
+  const decksList = Object.keys(decks).map(item => ({key: item}));
+  return (
+    <View style={styles.container}>
+      {decks && (
+        <FlatList
+          contentContainerStyle={styles.deckList}
+          data={decksList}
+          renderItem={({ item }) => (
+            <DeckListItem
+              deck={item.key}
+              cardsNumber={decks[item.key].questions.length}
+              navigation={navigation}
+            />
+          )}
+        />
+      )}
+      <TouchableOpacity
+        style={styles.addBtn}
+        onPress={() => navigation.navigate('CreateDeck')}
+      >
+        <Ionicons name='md-add-circle' size={55} color='#f83926' />
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
